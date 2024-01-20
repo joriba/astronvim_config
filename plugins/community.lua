d0d627bd173bcca38d5194fa6cc83f4266f41404
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-01-20 17:03:48 +0100

    Add custom plugins and change colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,11 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.editing-support.auto-save-nvim" },
+  -- { import = "astrocommunity.editing-support.dial-nvi::m" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  -- { import = "astrocommunity.utility.noice-nvim" },
 }
