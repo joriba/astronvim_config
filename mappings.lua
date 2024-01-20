3615e3b0cc0e520ab1e1d86b418beb59fca12a71
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-01-20 23:07:57 +0100

    Add better system clipboard interaction with <leader>s

--- mappings.lua
+++ mappings.lua
@@ -32,9 +32,16 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- System clipboard interaction
+    ["<leader>s"] = { "\"+", desc = "System clipboard" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
+  v = {
+    -- System clipboard interaction
+    ["<leader>s"] = { "\"+", desc = "System clipboard" },
+  }
 }
