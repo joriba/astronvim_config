082a118e27061c323e4bbbc0ab55ca6ab6f139f4
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-01-25 16:20:06 +0100

    add a few plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,8 +9,46 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  -- TokyoNight Color scheme
   {
     "folke/tokyonight.nvim",
     lazy = false
   },
+
+  -- Neoscroll, for animated scrolling
+  {
+    "karb94/neoscroll.nvim",
+    version = false,
+    event = "BufRead",
+    config = function()
+      require('neoscroll').setup {}
+    end
+  },
+
+  -- Split/Join function arguments onto multiple/single lines
+  {
+    'echasnovski/mini.splitjoin',
+    keys = {
+      {"gS", desc = "Split/Join arguments"},
+    },
+    version = false,
+    config = function () require('mini.splitjoin').setup() end
+  },
+
+  -- Highlight occurrences of word under cursor
+  {
+    'echasnovski/mini.cursorword',
+    event = "BufRead",
+    version = false,
+    config = function () require('mini.cursorword').setup() end
+  },
+
+  -- Generate documentation with :Neogen command
+  {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    cmd = "Neogen",
+    config = true,
+  }
 }
