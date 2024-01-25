d518f7e9afdfdb500ec3baee34988acc777cd268
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-01-25 23:03:10 +0100

    reenable auto save plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,7 +6,7 @@ return {
 
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  -- { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
   -- { import = "astrocommunity.editing-support.dial-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   -- { import = "astrocommunity.utility.noice-nvim" },
