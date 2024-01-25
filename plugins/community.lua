d641f2f24e55809059fab45ae1fcc33c0ae64716
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-01-25 23:08:12 +0100

    add color picker and highlighter plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -9,5 +9,6 @@ return {
   { import = "astrocommunity.editing-support.auto-save-nvim" },
   -- { import = "astrocommunity.editing-support.dial-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.color.ccc-nvim" },
   -- { import = "astrocommunity.utility.noice-nvim" },
 }
